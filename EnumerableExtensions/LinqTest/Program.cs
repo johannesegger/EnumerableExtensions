55670d8503cf89e5a4553821d34e9cd56644088e
Johannes Egger <j.egger@posteo.at>
2018-03-22 14:32:49 +0100

    Add LINQ test project

---
+++ EnumerableExtensions/LinqTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EnumerableExtensions;
+
+namespace LinqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new[] { "Johannes", "Geri", "Johannes", "Roland", "Daniel" };
+
+            // Imperativ
+            var filteredList = new List<string>();
+            foreach (var item in list)
+            {
+                if (item.Length > 5)
+                {
+                    filteredList.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(", ", filteredList));
+
+            // Deklarativ
+            var filteredList2 = list
+                .Where(item => item.Length > 5)
+                .ToList();
+            Console.WriteLine(string.Join(", ", filteredList2));
+
+        }
+    }
+}
